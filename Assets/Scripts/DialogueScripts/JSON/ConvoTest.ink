1da8db782f2217fe47e4af542a849e9ff17a374a
yokini <56157066+yokini@users.noreply.github.com>
2022-06-02 12:52:06 +0300

    New Dialogue Scene, pt I
    Trying out a different type of Dialogue system, previous one removed.
    Json file and UI created

--- Assets/Scripts/DialogueScripts/JSON/ConvoTest.ink
+++ Assets/Scripts/DialogueScripts/JSON/ConvoTest.ink
@@ -1,2 +1,10 @@
-Hello!
-What brings you here?!
+EXTERNAL Name(charName)
+
+{Name("River")}
+Let’s see what we’ve got here! Splitting up would make things faster. Robin, you can climb right? You take the tree, Skyler the *narrow place*. Being the smallest, I can access this *small place*
+{Name("Robin")}
+Alright. Let’s meet up here when we are done.
+{Name("River")}
+Noted! 
+{Name("Skyler")}
+... Guys please don’t get lost.
---
+++ Assets/Scripts/DialogueScripts/ScriptReader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
+
+public class ScriptReader : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset inkJsonFile;
+    private Story storyScript;
+
+    public TMP_Text dialogueText;
+    public TMP_Text nameTag;
+
+    void Start()
+    {
+        LoadStory();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextLine();
+        }
+    }
+
+    void LoadStory()
+    {
+        storyScript = new Story(inkJsonFile.text);
+        storyScript.BindExternalFunction("Name", (string charName) => ChangeName(charName));
+    }
+
+    public void DisplayNextLine()
+    {
+        if(storyScript.canContinue)
+        {
+            // Get next storyline
+            string text = storyScript.Continue();
+            // Remove white space
+            text = text?.Trim();
+            //Display
+            dialogueText.text = text;
+        }
+
+        else
+        {
+            dialogueText.text = "end of this convo";
+        }
+    }
+
+    public void ChangeName(string name)
+    {
+        string characterName = name;
+        nameTag.text = characterName;
+
+    }
+}
