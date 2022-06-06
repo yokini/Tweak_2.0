065cfaba6ec1cab44178e80c712efc872e864f8b
Chinmay Kulkarni <ckulkar1@tlu.ee>
2022-06-06 20:36:03 +0300

    some changes

--- Assets/Robin_Scripts/Boomerang.cs
+++ Assets/Robin_Scripts/Boomerang.cs
@@ -36,7 +36,7 @@ public class Boomerang : MonoBehaviour
 
         itemRotate = gameObject.transform;
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
 
         locationInFrontOfPlayer =  mousePosition * throwSpeed;
        
