228542ac13dc490500e9a56a0a4ba44384424197
Chinmay Kulkarni <ckulkar1@tlu.ee>
2022-06-06 20:38:24 +0300

    Revert "some changes"
    This reverts commit 065cfaba6ec1cab44178e80c712efc872e864f8b.

--- Assets/Robin_Scripts/Boomerang.cs
+++ Assets/Robin_Scripts/Boomerang.cs
@@ -36,7 +36,7 @@ public class Boomerang : MonoBehaviour
 
         itemRotate = gameObject.transform;
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
 
         locationInFrontOfPlayer =  mousePosition * throwSpeed;
        
