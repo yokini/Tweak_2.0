e04076017052cf4ddf5c3e154ef9f3927cc575c3
Chinmay Kulkarni <ckulkar1@tlu.ee>
2022-06-08 13:06:23 +0300

    UI popUp

---
+++ Assets/Scripts/Main/UIpopUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIpopUp : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public GameObject chooseObject;
+
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    public void OpenUI() 
+    {
+        canvas.SetActive(true);
+        chooseObject.SetActive(false);
+    }
+}
